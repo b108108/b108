bc657bb36b44f94aa996428e9dd3a0e4e618f15f
b108108 <b108@tut.by>
2017-06-25 07:26:34 +0300

    Update Json_data_client.rb

--- Json_data_client.rb
+++ Json_data_client.rb
@@ -1,7 +1,7 @@
 module ExportBin
   class JsonDataClient
     def self.load_json(export_bin)
-      # json_url="http://qa-gateway.cap.network/efficientm/exportbinjson/#{json_url}?apiKey=#{get_auth_token}"
+      # json_url="http://"
       # uri = URI.parse(json_url)
       # http = Net::HTTP.new(uri.host, uri.port)
       # data = http.request(Net::HTTP::Get.new(uri.request_uri))
@@ -16,7 +16,7 @@ module ExportBin
       https.use_ssl = true
       https.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request = Net::HTTP::Get.new(uri.request_uri)
-      request["X-Auth-Token"] = get_auth_token
+      request["..."] = get_auth_token
       response = https.request(request)
       while response.code == '301' || response.code == '302'
         return download_image(response.header['location'])
